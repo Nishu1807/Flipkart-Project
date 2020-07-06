7d80e3711c78b23268ab7409c721b68121dd19ab
Sadhana20 <zagadesadhana20@gmail.com>
2020-07-06 13:06:09 +0530

    new BannerComponant.java

--- src/main/java/com/flipkart/pages/home/BannerComponant.java
+++ src/main/java/com/flipkart/pages/home/BannerComponant.java
@@ -1,6 +1,52 @@
 package com.flipkart.pages.home;
 
+import org.openqa.selenium.By;
+
+import com.flipkart.pages.Keyword;
+
 public class BannerComponant {
- // Banner class for checking banner element
+ 
+	public void isSearchTextboxDisplay()
+	{
+		
+		if( Keyword.getDriver().findElement(By.xpath("//input[@class=\"LM6RPg\"]")).isDisplayed()){
+			System.out.println("Element is Displayed");
+			}else{
+			System.out.println("Element is Not Displayed");
+			}
+	
+	}
+	
+     public void isSearchTextboxEnable()
+	{
+		
+		if(Keyword.getDriver().findElement(By.xpath("//input[@class=\"LM6RPg\"]")).isEnabled()){
+			System.out.println("Element is Enable");
+			}else{
+			System.out.println("Element is Disabled");
+			}
+				
+	}
+	public void isSearchTextboxClicakble()
+	{
+		Keyword.getDriver().findElement(By.xpath("//button[@class=\"vh79eN\"]")).click();
+		
+		
+	}
+	public void isTextPresent()
+	{
+		if(Keyword.getDriver().getPageSource().contains("Search for products, brands and more")){
+			System.out.println("Text is present");
+			}else{
+			System.out.println("Text is absent");
+			}
+	}
+	
+	public void enterText()
+	{
+		Keyword.getDriver().findElement(By.xpath("//input[@class=\"LM6RPg\"]")).sendKeys("mobile");
+		
+	}
+	
 	
 }
