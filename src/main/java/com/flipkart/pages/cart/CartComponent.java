09fe9ce2d30ce712857102f73d758e46cf0929b5
monika chaudhari <chaudhari.moni789@gmail.com>
2020-07-08 11:21:33 +0530

    changes method name

--- src/main/java/com/flipkart/pages/cart/CartComponent.java
+++ src/main/java/com/flipkart/pages/cart/CartComponent.java
@@ -5,18 +5,17 @@ import org.openqa.selenium.By;
 import com.flipkart.pages.Keyword;
 
 public class CartComponent {
-	
-	By cart=By.xpath("//a[@class=\"_3ko_Ud\"]");
-	
-	By shopping=By.xpath("//button[@class='_2AkmmA _2Z-ax5 _1eFTEo']");
-	
+
+	By cart = By.xpath("//a[@class=\"_3ko_Ud\"]");
+
+	By shopping = By.xpath("//button[@class='_2AkmmA _2Z-ax5 _1eFTEo']");
+
 	public void clickOnCart() {
 		Keyword.getDriver().navigate().refresh();
 		Keyword.getDriver().findElement(cart).click();
 	}
-	
-	public void clickOnShopNowButton() throws InterruptedException {
-		Thread.sleep(3000);
-		Keyword.getDriver().findElement(shopping).click();
+
+	public void clickOnShopNow() throws InterruptedException {
 	}
+
 }
